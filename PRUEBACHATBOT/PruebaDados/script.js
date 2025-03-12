9333f75cc5c63cb868dc0b170c4879ac4f662625
Juanma <juanmavegacarrillo@gmail.com>
2025-03-12 12:20:08 +0100

    87p

--- PRUEBACHATBOT/PruebaDados/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('rollButton').addEventListener('click', function() {
-    const dice = document.getElementById('dice');
-    dice.style.animation = 'roll 2s ease-in-out forwards';
-    
-    // Reset animation after it completes
-    setTimeout(() => {
-        dice.style.animation = 'none';
-    }, 2000);
-});
